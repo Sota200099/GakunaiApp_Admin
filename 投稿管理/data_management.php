d5d27cc252ffeecec0e038adf9ba39a328458190
204tb <200204tb@yse-c.net>
2021-11-18 11:14:26 +0900

    投稿管理ページのデザイン修正

--- 投稿管理/data_management.php
+++ 投稿管理/data_management.php
@@ -34,10 +34,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="css/style.css">
+
     <title>Document</title>
 </head>
-<body>
+<body class="bg-lsBlue">
     <script src="js/paginathing.min.js"></script>
     <script src="js/jquery-3.6.0.min.js"></script>   
     <h2 style="text-align:center;" class="mb-5"><a href="./index.php">投稿管理ページ</a></h2>
@@ -47,7 +48,11 @@
             <?php foreach($logs as $value):?>
                 <form action="delete_post.php" method="post">
                     <?php if($value["delete_flag"]==0):?>
-                        <p class="mb-4"><?=$value["title"]?>　投稿者：<?=$value["name"]?>　<?=$value["date"]?><button type="submit" style="position:fixed;right:10%;" class="<?=$value["title"]?> btn btn-outline-primary" value="<?=$value["title"]?>" onclick="input_confirm()">投稿を削除</button></p>
+                        <div class="mb-5 card">
+                        <span class="card-body" style="width:70%;height:20%;font-size: 1.3em;">タイトル：<?=$value["title"]?>　投稿者：<?=$value["name"]?>　<?=$value["date"]?></span>
+                        <p class="ml-4" style="font-size: 1.3em;">本文：　<?=$value["text"]?></p>
+                        <button type="submit" class="<?=$value["title"]?> btn btn-outline-dark" value="<?=$value["title"]?>" onclick="input_confirm()">投稿を削除</button>
+                        </div>
                     <?php endif?>
                     <input type="hidden" name="post_title" value="<?=$value["title"]?>,<?=$value["name"]?>,<?=$value["date"]?>">
                 </form>
@@ -95,7 +100,7 @@
             $page_start=$page_numbers-4;
         }
     ?>
-    <div class="mt-5 pagination container d-flex justify-content-center">
+    <div class="pagination container d-flex justify-content-center" style="margin-top: 4%;">
         <?php echo '<button class="btn btn-primary mr-5 page-item"><a href ="./data_management.php?page_num='.($previous).'" style="color:white;">'."前へ".'</a></button>'?>
         <div class="buttons" style="text-align: center;">
             <?php for($i=$page_start;$i<=$page_amount;$i++){
--- 投稿管理/table_reset.php
+++ 投稿管理/table_reset.php
@@ -11,20 +11,16 @@ require_once "functions.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
-<body>
+<body class="bg-lsBlue">
     <div class="container">
-        <h3>テーブルの初期化</h3>
+        <h3 style="background-color: lightskyblue;">テーブルの初期化</h3>
         <p class="mt-5">管理者用のテーブル初期化機能</p>
-
         <form action="reset.php" class="mt-5 float-right">
             <button class="btn btn-primary">テーブルを初期化</button>
         </form>
-
     </div>
-
-
 </body>
 </html>
