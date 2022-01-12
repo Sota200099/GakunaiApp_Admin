21997156353855d92c1568425a6e7dc77094ec50
github <github>
2022-01-12 13:00:16 +0900

    テーブル名の重複チェック機能

---
+++ 時間割作成/index.php
@@ -0,0 +1,102 @@
+<?php
+
+//クラス名をすべて取得
+function Get_class_Id($pdo)
+{
+    try{
+        $sql = "SELECT class_id FROM classes ORDER BY class_id ASC";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $class_id = $stmt->fetchall(PDO::FETCH_ASSOC);
+        return $class_id;
+    }catch (PDOException $e) {
+        header('Location: error_page.php');
+        exit;
+    }
+}
+
+//class名の重複チェック
+function unique($pdo, $class)
+{
+    try{
+        $sql = 'SHOW TABLES FROM timetable';
+        //SQL文を実行
+        $table_stmt=$pdo->prepare($sql);
+        $table_stmt->execute();
+        //データベースのテーブルすべて読み出すまでループ
+        while($table_rec = $table_stmt->fetch(PDO::FETCH_ASSOC)){
+            //連想配列すべてを読み出すまでループ
+            foreach($table_rec as $key => $val){
+                // //クラス名が重複している場合trueを返す
+                if($class == $val){
+                    return $val;
+                }
+            }
+        }
+        return false;
+    }catch (PDOException $e) {
+        header('Location: error_page.php');
+        exit;
+    }
+
+}
+
+//DB接続
+require_once('Components/connect.php');
+
+//pdo変数
+$pdo_attendance = Attendance();
+$pdo_timetable = TimeTable();
+
+//エラーメッセージ用の変数
+$message = "";
+
+//クラス名をすべて取得
+$class_id = Get_class_Id($pdo_attendance);
+
+//postデータを受け取る
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    //テーブル名チェック
+    $isTableName = unique($pdo_timetable, $_POST['class']);
+    //選択したクラス名のテーブルが既に存在している場合
+    if ($isTableName) {
+        $message = "選択したクラス名のテーブルは既に存在しています";
+    }else{
+        //csv読み込み
+
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <title>時間割作成</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div class="error_message">
+            <?= $message ?>
+        </div> 
+        <div class="mt-2" name="class" id="class">
+            <label>学科・クラス</label>        
+            <select name="class" class="form-control" required>
+                <option value="" hidden>選択してください</option>
+                <?php for($i = 0; $i < count($class_id); $i++){
+                    echo "<option>{$class_id[$i]["class_id"]}</option>";
+                }?>
+            </select>
+        </div>
+        <div class="mt-4">
+            <button class="w-100 btn btn-lg btn-primary">作成</button>
+        </div>
+    </form>    
+</body>
+
+</html>
