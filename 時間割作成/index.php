ffe97879c3dbab287c5f219c882a3a35b720bba9
github <github>
2022-01-14 09:47:32 +0900

    テーブル作成と同時にcsvファイルをインポート

--- 時間割作成/index.php
+++ 時間割作成/index.php
@@ -46,8 +46,8 @@ function CreateTable($pdo, $class)
     // テーブル作成のSQLを作成
     $sql = "CREATE TABLE IF NOT EXISTS {$class} (
         times_id INT(11) PRIMARY KEY,
-        start_time  DATETIME NOT NULL,
-        ending_time DATETIME NOT NULL,
+        start_time  TIME NOT NULL,
+        ending_time TIME NOT NULL,
         subjects_id_monday VARCHAR(40),
         subjects_id_tuesday VARCHAR(40),
         subjects_id_wednesday VARCHAR(40),
@@ -62,10 +62,9 @@ function CreateTable($pdo, $class)
 
     // SQLを実行
     $stmt = $pdo->query($sql);
-    return $stmt;
 }
 
-function InportCsv($pdo,$class,$aaa)
+function ImportCsv($pdo,$class,$aaa)
 {
     try{
         $sql = "INSERT INTO {$class} VALUES(\"$aaa\");";
@@ -93,7 +92,6 @@ $class_id = Get_class_Id($pdo_attendance);
 //postデータを受け取る
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-    var_dump($_FILES);
     $class = $_POST['class'];
     //テーブル名チェック
     $isTableName = unique($pdo_timetable, $class);
@@ -103,23 +101,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     }else{
         //テーブル作成
         CreateTable($pdo_timetable, $class);
-        $message = "テーブル作成に成功しました";
         //fopen()関数で、ファイルを開く。"r"で「読み込み専用」という指示。
         $tmp = fopen($_FILES['csvfile']['tmp_name'], "r");
         //2次元配列にする。fgetcsv()関数により、csvデータを読み込む。
         while ($csv[] = fgetcsv($tmp, "1024")) {}
         //文字化け防止のため、配列 $csv の文字コードをSJIS-winからUTF-8に変換
-        mb_convert_variables("UTF-8", "SJIS-win", $csv);
+        mb_convert_encoding($csv, "SJIS", "UTF-8");       
         $lim = count($csv);//for文で読み込むために、配列の最大行数を出す
         for($i=0; $i<=$lim; $i++){
             if($i < ($lim-1)){
                 $ar = $csv[$i];
                 $aaa = implode("\",\"", $ar);
+                //csvインポート
+                ImportCsv($pdo_timetable,$class,$aaa);
             }
         }
-        //csvインポート
-        InportCsv($pdo_timetable,$class,$aaa);
-        $message = "テーブル作成に失敗しました";
     }
 }
 
