7ce34bf2b7c6ea8ee4ca9e794df0b00ab34e89c1
github <github>
2022-01-14 13:32:21 +0900

    一旦完成

--- 時間割作成/index.php
+++ 時間割作成/index.php
@@ -45,7 +45,7 @@ function CreateTable($pdo, $class)
 {
     // テーブル作成のSQLを作成
     $sql = "CREATE TABLE IF NOT EXISTS {$class} (
-        times_id INT(11) PRIMARY KEY,
+        times_id INT(11) AUTO_INCREMENT PRIMARY KEY,
         start_time  TIME NOT NULL,
         ending_time TIME NOT NULL,
         subjects_id_monday VARCHAR(40),
@@ -67,7 +67,7 @@ function CreateTable($pdo, $class)
 function ImportCsv($pdo,$class,$aaa)
 {
     try{
-        $sql = "INSERT INTO {$class} VALUES(\"$aaa\");";
+        $sql = "INSERT INTO {$class} VALUES(times_id,\"$aaa\");";
         // SQLを実行
         $stmt = $pdo->query($sql);
     }catch (PDOException $e) {
@@ -95,26 +95,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     $class = $_POST['class'];
     //テーブル名チェック
     $isTableName = unique($pdo_timetable, $class);
-    //選択したクラス名のテーブルが既に存在している場合
+    //選択したクラス名のテーブルの有無を判定
     if ($isTableName) {
-        $message = "選択したクラス名のテーブルは既に存在しています";
+        $message = "既に作成されているテーブルにファイルデータをインポートしました";
     }else{
         //テーブル作成
         CreateTable($pdo_timetable, $class);
-        //fopen()関数で、ファイルを開く。"r"で「読み込み専用」という指示。
-        $tmp = fopen($_FILES['csvfile']['tmp_name'], "r");
-        //2次元配列にする。fgetcsv()関数により、csvデータを読み込む。
-        while ($csv[] = fgetcsv($tmp, "1024")) {}
-        //文字化け防止のため、配列 $csv の文字コードをSJIS-winからUTF-8に変換
-        mb_convert_encoding($csv, "SJIS", "UTF-8");       
-        $lim = count($csv);//for文で読み込むために、配列の最大行数を出す
-        for($i=0; $i<=$lim; $i++){
-            if($i < ($lim-1)){
-                $ar = $csv[$i];
-                $aaa = implode("\",\"", $ar);
-                //csvインポート
-                ImportCsv($pdo_timetable,$class,$aaa);
-            }
+        $message = "選択したクラス名のテーブルを新たに作成し、ファイルデータをインポートしました";
+    }
+
+    //fopen()関数で、ファイルを開く。"r"で「読み込み専用」という指示。
+    $tmp = fopen($_FILES['csvfile']['tmp_name'], "r");
+    //2次元配列にする。fgetcsv()関数により、csvデータを読み込む。
+    while ($csv[] = fgetcsv($tmp, "1024")) {}
+    //文字化け防止のため、配列 $csv の文字コードをSJIS-winからUTF-8に変換
+    mb_convert_encoding($csv, "SJIS", "UTF-8");       
+    $lim = count($csv);//for文で読み込むために、配列の最大行数を出す
+    for($i=0; $i<=$lim; $i++){
+        if($i < ($lim-1)){
+            $ar = $csv[$i];
+            $aaa = implode("\",\"", $ar);
+            //csvインポート
+            ImportCsv($pdo_timetable,$class,$aaa);
         }
     }
 }
